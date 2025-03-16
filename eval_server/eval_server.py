ad1f344fc3ea156ec2a9475eddfc1d0bd4fc3aa2
Eyal Roginski <eyalrog1@gmail.com>
2025-03-16 19:28:13 +0200

    Fix eval_server API

--- eval_server/eval_server.py
+++ eval_server/eval_server.py
@@ -1,3 +1,6 @@
+from typing import Callable
+
+
 class Server:
-    def add_test(self, name: str, metadata: dict):
+    def add_test(self, name: str, tester: Callable[[str], bool], metadata: dict):
         pass
